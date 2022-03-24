1ce9e2cc4bbce7c88b1467a368d95c0414547366
Michael O'Connor <oconnor97@aol.com>
2022-03-24 10:54:38 -0400

    importing Home component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
 import React from 'react';
 // Components
 import Header from './components/Header/index';
+import Home from "./components/Home";
 
 // styles
-import { GlobalStyle } from './GlobalStyle';
+import { GlobalStyle } from "./GlobalStyle";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Home />
       <GlobalStyle />
     </div>
-    
   );
 }
 

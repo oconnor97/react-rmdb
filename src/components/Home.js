7cbbfd592ecbec6f6fb5f3ea76a396197dc490c7
Michael O'Connor <oconnor97@aol.com>
2022-03-24 10:55:05 -0400

    creatting a useEffect to console log returned movie data

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,6 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+// API
+import API from "../API";
 // Config
 import { POSTER_SIZE, BACKDROP_SIZE, IMAGE_BASE_URL } from "../config";
 // Components
@@ -12,6 +14,22 @@ const Home = () => {
   const [state, setState] = useState();
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(false);
+
+  const fetchMovies = async (page, searchTerm = "") => {
+    try {
+      setError(false);
+      setLoading(true);
+
+      const movies = await API.fetchMovies(searchTerm, page);
+      console.log(movies);
+    } catch (error) {
+      setError(true);
+    }
+  };
+  // Intial Render
+  useEffect(() => {
+    fetchMovies(1);
+  }, []);
 
   return <div>Home Page</div>;
 };

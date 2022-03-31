9b1af91d376bc592b79025e3053dacaf37abdc6d
Michael O'Connor <oconnor97@aol.com>
2022-03-31 14:11:58 -0400

    creating a new SearchBar component and the SearchBar styles.js

---
+++ src/components/SearchBar/index.js
@@ -0,0 +1,7 @@
+import React, { useState, useEffect, useRef } from "react";
+
+// Image
+import searchIcon from "../../images/search-icon.svg";
+
+// Styles
+import { Wrapper, Content } from "./SearchBar.styles";

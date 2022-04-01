73d37f0661ed023e1855208f4db071c29b4bc2dd
Michael O'Connor <oconnor97@aol.com>
2022-04-01 11:53:40 -0400

    creating 2 new components to redirect to when a movie is clicked or not found

--- src/components/Thumbnail/index.js
+++ src/components/Thumbnail/index.js
@@ -1,12 +1,21 @@
 import React from "react";
 
+// Router
+import { Link } from 'react-router-dom';
+
 // Styles
 import { Image } from "./Thumbnail.styles";
 
 const Thumbnail = ({ image, movieId, clickable }) => {
   return (
     <div>
-      <Image src={image} alt="movie thumbnail" />
+      {clickable ? (
+        <Link to={`/${movieId}`}>
+          <Image src={image} alt="movie thumbnail" />
+        </Link>
+      ) : (
+        <Image src={image} alt="movie thumbnail" />
+      )}
     </div>
   );
 };
